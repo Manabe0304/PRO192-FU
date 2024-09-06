a3ff9f22a2d73cc39618daf649c50c5109ad479c
Manabe0304 <hainamn833@gmail.com>
2024-09-06 13:30:20 +0700

    Create Exercise4.java

---
+++ Slot 2/Team 3/Exercise4.java
@@ -0,0 +1,11 @@
+package Q4;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "banana", "cherry"};
+
+        for (String fruit : fruits) {
+            System.out.print(fruit + ", ");
+        }
+    }
+}
