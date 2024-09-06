79a3ca74c96ca21006a85d934c7d572f818f4d71
Manabe0304 <hainamn833@gmail.com>
2024-09-06 10:31:10 +0700

    Create Exercise1.java

---
+++ Slot 2/Team 3/Exercise1.java
@@ -0,0 +1,18 @@
+package Q1;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        
+        do {
+            System.out.print("Enter a number: ");
+            number = sc.nextInt();
+        } while (number != 0);
+        
+        System.out.println("Loop Exited");
+    }
+}
