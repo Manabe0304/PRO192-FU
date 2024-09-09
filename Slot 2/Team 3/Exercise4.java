af9560ba1345cdf2645b64c9d914a234674c059e
Manabe0304 <hainamn833@gmail.com>
2024-09-09 22:51:07 +0700

    Update Exercise4.java

--- Slot 2/Team 3/Exercise4.java
+++ Slot 2/Team 3/Exercise4.java
@@ -1,11 +1,20 @@
 package Q4;
 
-public class Exercise4 {
-    public static void main(String[] args) {
-        String[] fruits = {"apple", "banana", "cherry"};
+import java.util.Scanner;
 
-        for (String fruit : fruits) {
-            System.out.print(fruit + ", ");
+public class UserInputArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        sc.nextLine();  // Consume the newline character
+        String[] a = new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("String " + (i + 1) + ": ");
+            a[i] = sc.nextLine();  
+        }
+        for (String string : a) {
+            System.out.println(string); 
         }
     }
 }
+
