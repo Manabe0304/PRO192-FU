051bed9afcc435a1129a8809d141b8e713bbc3d9
Manabe0304 <hainamn833@gmail.com>
2024-09-09 23:39:35 +0700

    Update Exercise2.java

--- Slot 2/Team 3/Exercise2.java
+++ Slot 2/Team 3/Exercise2.java
@@ -3,26 +3,28 @@ import java.util.Scanner;
 
 public class Exercise2 {
     public int sum(int a, int b) {
-        return a+b;
+        return a + b;
     }
+
     public double sum(double a, double b) {
-        return a+b;
+        return a + b;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Exercise2 exercise = new Exercise2();
 
-        System.out.print("Enter first integer: ");
+        System.out.print("Enter two integers: ");
         int intA = sc.nextInt();
-        System.out.print("Enter second integer: ");
         int intB = sc.nextInt();
 
-        System.out.print("Enter first double: ");
+        System.out.print("Enter two doubles: ");
         double doubleA = sc.nextDouble();
-        System.out.print("Enter second double: ");
         double doubleB = sc.nextDouble();
 
         System.out.println("Sum of integers: " + exercise.sum(intA, intB));
         System.out.println("Sum of doubles: " + exercise.sum(doubleA, doubleB));
+
+        sc.close();
     }
 }
