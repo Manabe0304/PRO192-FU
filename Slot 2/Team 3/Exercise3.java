06325d0b02c4053ec6e29f9b6a555ee3d4137ba1
Manabe0304 <hainamn833@gmail.com>
2024-09-09 20:59:56 +0700

    Update Exercise3.java

--- Slot 2/Team 3/Exercise3.java
+++ Slot 2/Team 3/Exercise3.java
@@ -7,16 +7,15 @@ public class Exercise3 {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
         int sum = 0;
-		for (int i = 0; i < 10; i++) {
-			System.out.print("a[" + i + "] = " );
-			arr[i] = sc.nextInt();
-		}
+	for (int i = 0; i < 10; i++) {
+		System.out.print("a[" + i + "] = " );
+		arr[i] = sc.nextInt();
+	}
 		
         for (int i = 0; i < arr.length; i++) {
              sum += arr[i];
         } 
         System.out.println("Sum of array is: " + sum);
-    
     }
 }
     
