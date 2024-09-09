c60f4451b8159d990d75778f130d92dc751a469b
Manabe0304 <hainamn833@gmail.com>
2024-09-09 22:57:01 +0700

    Create Exercise1.java

---
+++ Slot 2/Team 1/Exercise1.java
@@ -0,0 +1,16 @@
+package Q1
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 != 0) {
+            System.out.print("Odd");
+        }
+        else {
+            System.out.print("Even");
+        }
+    }
+}
