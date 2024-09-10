982677591fd4836c729d998c2d426b2bf3601160
Manabe0304 <hainamn833@gmail.com>
2024-09-10 09:19:50 +0700

    Create Exercise2.java

---
+++ Slot 2/Team 1/Exercise2.java
@@ -0,0 +1,18 @@
+package Q2
+
+import java.util.Scanner;
+
+public class Exercise2 {
+    public int sum (int a, int b) {
+        return a + b;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Exercise2 obj = new Exercise2();
+        System.out.print("Nhap a va b: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Tong cua 2 so la: " + obj.sum(a, b));
+    }
+}
