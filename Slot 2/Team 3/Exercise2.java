e7e05d8173214b8bb702b3feec1b31cb09e5cda3
Manabe0304 <hainamn833@gmail.com>
2024-09-10 09:14:35 +0700

    Update Exercise2.java

--- Slot 2/Team 3/Exercise2.java
+++ Slot 2/Team 3/Exercise2.java
@@ -12,18 +12,18 @@ public class Exercise2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Exercise2 exercise = new Exercise2();
+        Exercise2 obj = new Exercise2();
 
         System.out.print("Enter two integers: ");
-        int intA = sc.nextInt();
-        int intB = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
         System.out.print("Enter two doubles: ");
-        double doubleA = sc.nextDouble();
-        double doubleB = sc.nextDouble();
+        double c = sc.nextDouble();
+        double d = sc.nextDouble();
 
-        System.out.println("Sum of integers: " + exercise.sum(intA, intB));
-        System.out.println("Sum of doubles: " + exercise.sum(doubleA, doubleB));
+        System.out.println("Sum of integers: " + obj.sum(a, b));
+        System.out.println("Sum of doubles: " + obj.sum(c, d));
 
         sc.close();
     }
