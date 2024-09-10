660a3b7a6b837c57096ce8180f0b5f71843d8ca3
Manabe0304 <hainamn833@gmail.com>
2024-09-10 09:10:35 +0700

    Update Exercise1.java

--- Slot 2/Team 3/Exercise1.java
+++ Slot 2/Team 3/Exercise1.java
@@ -7,7 +7,6 @@ public class Exercise1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number;
-        
         do {
             System.out.print("Enter a number: ");
             number = sc.nextInt();
