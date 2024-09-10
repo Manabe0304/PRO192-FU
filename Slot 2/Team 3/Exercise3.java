aaeec1467461925179b8b59599389ce0ddab422a
Manabe0304 <hainamn833@gmail.com>
2024-09-10 21:50:59 +0700

    Update Exercise3.java

--- Slot 2/Team 3/Exercise3.java
+++ Slot 2/Team 3/Exercise3.java
@@ -8,7 +8,8 @@ public class Exercise3 {
         int[] arr = new int[10];
         int sum = 0;
 	for (int i = 0; i < 10; i++) {
-		arr[i] = sc.nextInt();
+             System.out.print("a[" + i + "] = ");
+	     arr[i] = sc.nextInt();
 	}
 		
         for (int i = 0; i < arr.length; i++) {
