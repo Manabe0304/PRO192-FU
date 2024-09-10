772289acac2e63e7b30f89a738aa9eb029fab47a
Manabe0304 <hainamn833@gmail.com>
2024-09-10 22:04:54 +0700

    Create Exercise4.java

---
+++ Slot 2/Team 1/Exercise4.java
@@ -0,0 +1,11 @@
+package Q4
+
+public class Execise4 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 50; i++) {
+			if (i % 5 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
