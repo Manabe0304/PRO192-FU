6f64b2a1de57c66c13ee5b5d78d55e866455692d
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:42:24 +0700

    Create Bai3.java

---
+++ Slot 4/Team 2/Bai3.java
@@ -0,0 +1,8 @@
+public class Bai3 {
+    public static void main(String[] args) {
+        // Gợi ý: Sử dụng vòng lặp for để in ra các số từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
