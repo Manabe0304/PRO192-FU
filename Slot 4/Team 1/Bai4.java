031d45d40c31de5d1016bb7d107357cee77146da
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:37:48 +0700

    Create Bai4.java

---
+++ Slot 4/Team 1/Bai4.java
@@ -0,0 +1,11 @@
+public class Bai4 {
+    // Biến toàn cục
+    static int x = 100;
+
+    public static void main(String[] args) {
+        // Gợi ý: Khai báo biến cục bộ trong phương thức
+        int y = 50;
+        System.out.println("Biến toàn cục x: " + x);
+        System.out.println("Biến cục bộ y: " + y);
+    }
+}
