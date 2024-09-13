8429cf4e6e1d491cda2fdd886f329729b3dfcfc2
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:37:26 +0700

    Create Bai3.java

---
+++ Slot 4/Team 1/Bai3.java
@@ -0,0 +1,11 @@
+public class Bai3 {
+    public static void main(String[] args) {
+        // Gợi ý: Khai báo các kiểu dữ liệu cơ bản và in ra giá trị của chúng
+        int a = 10;
+        double b = 5.5;
+        boolean c = true;
+        System.out.println("Giá trị của a: " + a);
+        System.out.println("Giá trị của b: " + b);
+        System.out.println("Giá trị của c: " + c);
+    }
+}
