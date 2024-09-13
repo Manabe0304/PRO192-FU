ae5696f96cb1d37861c400969a37046b855bbae8
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:38:32 +0700

    Create Bai6.java

---
+++ Slot 4/Team 1/Bai6.java
@@ -0,0 +1,12 @@
+public class Bai6 {
+    public static void main(String[] args) {
+        // Gợi ý: Khởi tạo một chuỗi và sử dụng các phương thức của lớp String
+        String chuoi = "Xin chào Java";
+        
+        // Lấy độ dài chuỗi
+        System.out.println("Độ dài của chuỗi: " + chuoi.length());
+
+        // Lấy ký tự tại vị trí thứ 0
+        System.out.println("Ký tự tại vị trí 0: " + chuoi.charAt(0));
+    }
+}
