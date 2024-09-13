8a36cd05dffa440b04b7d126322b4300eb9c1412
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:36:33 +0700

    Create Bai1.java

---
+++ Slot 4/Team 1/Bai1.java
@@ -0,0 +1,6 @@
+public class Bai1 {
+    public static void main(String[] args) {
+        // Gợi ý: Sử dụng System.out.println để in ra "Xin chào Java"
+        System.out.println("Xin chào Java");
+    }
+}
