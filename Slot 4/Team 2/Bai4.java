c93283c294f6f7cbd6eaa9f38f8caf0493d7d7b4
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:42:57 +0700

    Create Bai4.java

---
+++ Slot 4/Team 2/Bai4.java
@@ -0,0 +1,10 @@
+public class Bai4 {
+    public static void main(String[] args) {
+        // Gợi ý: Sử dụng vòng lặp while để in ra các số từ 1 đến 10
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
