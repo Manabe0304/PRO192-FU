11131c08efee7c820fb4c6f519c643af8778a620
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:38:11 +0700

    Create Bai5.java

---
+++ Slot 4/Team 1/Bai5.java
@@ -0,0 +1,12 @@
+public class Bai5 {
+    public static void main(String[] args) {
+        // Gợi ý: Khai báo các biến số nguyên và sử dụng các toán tử số học để thực hiện phép tính
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Tổng: " + (a + b));  // Phép cộng
+        System.out.println("Hiệu: " + (a - b));  // Phép trừ
+        System.out.println("Tích: " + (a * b));  // Phép nhân
+        System.out.println("Thương: " + (a / b)); // Phép chia
+    }
+}
