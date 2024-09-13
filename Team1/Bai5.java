f7ec3681ac92d22339fde5abb51a6948551f9f57
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:21:52 +0700

    Add files via upload

---
+++ Team1/Bai5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Team1;
+
+/**
+ *
+ * @author Asus
+ */
+public class Bai5 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Tong: " + (a + b));  
+        System.out.println("Hieu: " + (a - b));  
+        System.out.println("Tich: " + (a * b));  
+        System.out.println("Thuong: " + (a / b)); 
+    }
+}
