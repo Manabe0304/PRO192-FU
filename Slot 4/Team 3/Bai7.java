c00fbd3ddb6fdc489f568c6955a3a125ef0e5006
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:47:11 +0700

    Create Bai7.java

---
+++ Slot 4/Team 3/Bai7.java
@@ -0,0 +1,27 @@
+public class Bai7 {
+    // Gợi ý: Khai báo các thuộc tính với các phạm vi truy cập khác nhau
+    public String publicAttribute = "Public";
+    private String privateAttribute = "Private";
+    protected String protectedAttribute = "Protected";
+    String defaultAttribute = "Default"; // Không có phạm vi truy cập (mặc định)
+
+    // Phương thức để in giá trị các thuộc tính trong cùng lớp
+    public void hienThiPhamViTruyCap() {
+        System.out.println("Public: " + publicAttribute);
+        System.out.println("Private: " + privateAttribute);
+        System.out.println("Protected: " + protectedAttribute);
+        System.out.println("Default: " + defaultAttribute);
+    }
+
+    public static void main(String[] args) {
+        Bai7 obj = new Bai7();
+        obj.hienThiPhamViTruyCap();
+
+        // Kiểm tra truy cập từ lớp ngoài
+        System.out.println("Truy cập từ lớp ngoài:");
+        System.out.println("Public: " + obj.publicAttribute);
+        // System.out.println("Private: " + obj.privateAttribute); // Lỗi vì thuộc tính private
+        System.out.println("Protected: " + obj.protectedAttribute);
+        System.out.println("Default: " + obj.defaultAttribute);
+    }
+}
