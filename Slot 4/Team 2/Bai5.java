5b65d3feda2a387de59cb9c8b38bc41a6a203dc3
Manabe0304 <hainamn833@gmail.com>
2024-09-13 10:43:16 +0700

    Create Bai5.java

---
+++ Slot 4/Team 2/Bai5.java
@@ -0,0 +1,11 @@
+public class Bai5 {
+    public static void main(String[] args) {
+        // Gợi ý: Khai báo mảng một chiều gồm 5 số nguyên
+        int[] mangSo = {1, 2, 3, 4, 5};
+
+        // Sử dụng vòng lặp để in các phần tử của mảng
+        for (int i = 0; i < mangSo.length; i++) {
+            System.out.println("Phần tử tại vị trí " + i + " là: " + mangSo[i]);
+        }
+    }
+}
