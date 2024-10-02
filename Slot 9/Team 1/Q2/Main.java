0523ea5c0fffde43891720a8349808ea15a7e774
Manabe0304 <hainamn833@gmail.com>
2024-10-03 00:25:27 +0700

    Add files via upload

---
+++ Slot 9/Team 1/Q2/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Call the static method to calculate the square of 7
+        int result1 = MathOperations.square(7);
+        System.out.println("Square of 7: " + result1);
+
+        // Call the static method to calculate the square of 10
+        int result2 = MathOperations.square(10);
+        System.out.println("Square of 10: " + result2);
+    }
+}
---
+++ Slot 9/Team 1/Q2/MathOperations.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public interface MathOperations {
+    // Static method to calculate the square of a number
+    public static int square(int number) {
+        // To be implemented
+        return number * number;
+    }
+}
