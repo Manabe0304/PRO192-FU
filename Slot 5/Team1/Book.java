17c7a801896298a2ccbfe14345fa73e995b5ee98
Manabe0304 <hainamn833@gmail.com>
2024-10-12 19:25:16 +0700

    Update Book.java

--- Slot 5/Team1/Book.java
+++ Slot 5/Team1/Book.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Team1;
 
-/**
- *
- * @author Asus
- */
 public class Book {
     // Attributes
     private String title;
