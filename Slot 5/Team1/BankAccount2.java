dd79ad58a5d62e57bbc7b3912d259acf05754cf1
Manabe0304 <hainamn833@gmail.com>
2024-10-12 19:25:05 +0700

    Update BankAccount2.java

--- Slot 5/Team1/BankAccount2.java
+++ Slot 5/Team1/BankAccount2.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Team1;
 
-/**
- *
- * @author Asus
- */
 public class BankAccount2 {
     // Private fields
     private String accountNumber;
