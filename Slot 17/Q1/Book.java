0a82a60f8b4a37874d8c152e734873a16cb21995
Manabe0304 <hainamn833@gmail.com>
2024-11-01 10:38:29 +0700

    Create Book.java

---
+++ Slot 17/Q1/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String title;
+    private int price;
+    
+    public Book(){
+        this.title = "";
+        this.price = 0;
+    }
+    
+    public Book(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title.toUpperCase();
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
